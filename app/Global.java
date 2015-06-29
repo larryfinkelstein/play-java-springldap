5f6fab40ae866207c374b2b340524a7a94298fec
larryf1 <larryf@cspcorp.com>
2015-06-28 22:27:48 -0400

    Add formatter for javax.naming.name class

--- app/Global.java
+++ app/Global.java
@@ -1,4 +1,11 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.ldap.core.DistinguishedName;
+import org.springframework.ldap.support.LdapNameBuilder;
+
+import java.util.Locale;
+
+import javax.naming.Name;
+
 import org.springframework.context.ApplicationContext;
 import play.GlobalSettings;
 import play.Application;
@@ -6,6 +13,7 @@ import play.Application;
 import configs.AppConfig;
 import configs.LdapConfig;
 import play.Logger;
+import play.data.format.Formatters;
 
 public class Global extends GlobalSettings {
 
@@ -15,6 +23,19 @@ public class Global extends GlobalSettings {
     public void onStart(Application app) {
     	ctx = new AnnotationConfigApplicationContext(AppConfig.class, LdapConfig.class);
     	Logger.info("Starting application context "+ctx);
+    	
+    	Formatters.register(Name.class, new Formatters.SimpleFormatter<Name>() {
+            @Override
+            public Name parse(String input, Locale l)  {
+                return LdapNameBuilder.newInstance(input).build();
+            }   
+
+            @Override
+            public String print(Name name, Locale l) {
+                return name.toString();
+            }
+        });
+    	Logger.info("Registered Name Formatter");
     }
 
     //@Override
