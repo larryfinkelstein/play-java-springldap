1123d050bfaeb5687ae8a47828b074aff70a5a10
larryf1 <larryf@cspcorp.com>
2015-06-29 13:48:21 -0400

    Corrections/new tests for Application and Integration

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -1,47 +1,87 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import static org.junit.Assert.*;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.callAction;
+import static play.test.Helpers.charset;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.status;
+
+import java.util.Collections;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
+import controllers.Application;
 
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import play.mvc.Http;
+import play.mvc.Http.Request;
+import play.mvc.Result;
+import play.api.mvc.RequestHeader;
+import play.test.FakeApplication;
+import play.test.FakeRequest;
+import play.test.Helpers;
 import play.twirl.api.Content;
 
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
-
-
-/**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
 public class ApplicationTest {
+	
+	public static FakeApplication app;
+	
+	@BeforeClass
+	public static void startApp() {
+		app = Helpers.fakeApplication();
+		Helpers.start(app);
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		Map<String, String> flashData = Collections.emptyMap();
+		Map<String, Object> argData = Collections.emptyMap();
+		Long id = 2L;
+		play.api.mvc.RequestHeader header = mock(RequestHeader.class);
+		Request request = null;
+		Http.Context context = new Http.Context(id, header, request, flashData, flashData, argData);
+		Http.Context.current.set(context);
+	}
+	
+  @Test
+  public void testIndex() {
+    Result result = new Application().index();
+    assertEquals(OK, status(result));
+    assertEquals("text/html", contentType(result));
+    assertEquals("utf-8", charset(result));
+    assertTrue(contentAsString(result).contains("Spring LDAP"));
+  }
+	
+	@Test
+	public void testCallIndex() {
+	  Result result = callAction(
+	    controllers.routes.ref.Application.index(),
+	    new FakeRequest(GET, "/")
+	  );
+	  assertEquals(OK, status(result));
+	}
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertEquals(2, a);
+	private RequestHeader mock(Class<RequestHeader> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Test
+	public void renderTemplate() {
+		Content html = views.html.index.render("filter", null);
+		assertEquals("text/html", contentType(html));
+		assertTrue(contentAsString(html).contains("Spring LDAP Example"));
+	}
+  
+	@AfterClass
+    public static void stopApp() {
+        Helpers.stop(app);
     }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertEquals("text/html", html.contentType());
-        assertTrue(contentAsString(html).contains("Your new application is ready."));
-    }
-
 
 }
